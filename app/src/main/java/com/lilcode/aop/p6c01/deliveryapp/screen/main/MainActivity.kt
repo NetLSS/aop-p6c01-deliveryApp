073bf9aac07e2d9a0c82e41401c067210cf67d00
sslee <sangsulee@ebay.com>
2022-02-21 16:58:52 +0900

    Refactor MainActivity.kt 패키지 변경

--- app/src/main/java/com/lilcode/aop/p6c01/deliveryapp/screen/main/MainActivity.kt
+++ app/src/main/java/com/lilcode/aop/p6c01/deliveryapp/screen/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.lilcode.aop.p6c01.deliveryapp
+package com.lilcode.aop.p6c01.deliveryapp.screen.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lilcode.aop.p6c01.deliveryapp.R
 
 class MainActivity : AppCompatActivity() {
 
